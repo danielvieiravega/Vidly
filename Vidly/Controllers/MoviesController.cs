9db0b6b4060d145f806722a27969ab273981a444
Daniel Vieira Vega <daniel.vega@acad.pucrs.br>
2017-02-09 23:53:02 -0200

    First commit

---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,20 @@
+﻿using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies/Random
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Shrek!" };
+
+            //return View(movie);
+            //return Content("Hellow world");
+            //return HttpNotFound();
+            //return new EmptyResult();
+            return RedirectToAction("Index", "Home", new {page = 1, sortBy= "name"});
+        }
+    }
+}
