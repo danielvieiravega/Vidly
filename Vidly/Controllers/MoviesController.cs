8ec0add3d9143692227dee8c4e23a6d201994d54
Daniel Vieira Vega <daniel.vega@acad.pucrs.br>
2017-02-11 21:28:14 -0200

    Aula 15

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -1,5 +1,7 @@
-﻿using System.Web.Mvc;
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
 using Vidly.Models;
+using Vidly.ViewModels;
 
 namespace Vidly.Controllers
 {
@@ -9,12 +11,24 @@ namespace Vidly.Controllers
         public ActionResult Random()
         {
             var movie = new Movie() { Name = "Shrek!" };
+            var customers = new List<Customer>
+            {
+                new Customer {Name = "Customer 1" },
+                new Customer {Name = "Customer 2" }
+            };
+
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
 
             //return View(movie);
             //return Content("Hellow world");
             //return HttpNotFound();
             //return new EmptyResult();
-            return RedirectToAction("Index", "Home", new {page = 1, sortBy= "name"});
+            //return RedirectToAction("Index", "Home", new {page = 1, sortBy= "name"});
+            return View(viewModel);
         }
     }
 }
